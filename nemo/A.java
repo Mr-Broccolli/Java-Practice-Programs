774d76aa670fdf34fa2a15a8cc3eb537afbc3f55
Mr Broccoli <bhatnagar.nitigya13btt@gmail.com>
2026-05-30 23:07:44 +0530

    Delete nemo directory

--- nemo/A.java
+++
@@ -1,7 +0,0 @@
-package nemo;
-public class A {
-    void add(int a, int b)
-    {
-        System.out.println(a+b);
-    }
-}
--- nemo/S.java
+++
@@ -1,7 +0,0 @@
-package nemo;
-public class S {
-    void sub(int a, int b)
-    {
-        System.out.println(a-b);
-    }
-}
