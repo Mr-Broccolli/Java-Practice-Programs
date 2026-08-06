e129d4a154fa8ca34a137d4217b2923546b13be6
Mr Broccoli <bhatnagar.nitigya13btt@gmail.com>
2026-08-07 00:01:59 +0530

    Change Class Name and variables

---
+++ Arrays/Mixer_old.java
@@ -0,0 +1,70 @@
+package Arrays;
+import java.util.Scanner;
+public class Mixer_old {
+    int arr[];
+    int n;
+
+    Mixer_old(int nn) {
+        n = nn;
+        arr = new int[n];
+    }
+
+    void accept() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        for(int i = 0; i < n - 1; i++)
+            for(int j = 0; j < n - i - 1; j++)
+                if(arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+    }
+
+    Mixer_old mix(Mixer_old A) {
+        Mixer_old result = new Mixer_old(n + A.n);
+        int i = 0, j = 0, k = 0;
+
+        while(i < n && j < A.n) {
+            if(arr[i] < A.arr[j])
+                result.arr[k++] = arr[i++];
+            else if(arr[i] > A.arr[j])
+                result.arr[k++] = A.arr[j++];
+            else {
+                result.arr[k++] = arr[i++];
+                j++;
+            }
+        }
+
+        while(i < n)
+            result.arr[k++] = arr[i++];
+
+        while(j < A.n)
+            result.arr[k++] = A.arr[j++];
+
+        result.n = k;
+        return result;
+    }
+    void display() {
+        System.out.println("Mixed sorted array:");
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of first array: ");
+        Mixer_old A = new Mixer_old(sc.nextInt());
+        A.accept();
+        System.out.print("Enter size of second array: ");
+        Mixer_old B = new Mixer_old(sc.nextInt());
+        B.accept();
+
+        Mixer_old C = A.mix(B);
+        C.display();
+    }
+}
